59338c79fc22a40cb5f5b4d3c986dfe847cb96ed
Yi Cao <me@ycao.top>
2023-12-18 22:03:52 +0800

    Initial commit

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "fractal-clock")
+(define deps '("base" "gui" "date"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("scribblings/fractal-clock.scrbl" ())))
+(define pkg-desc "Simple fractal clock")
+(define version "0.0")
+(define pkg-authors '(xiaoyu2006))
+(define license '(MIT))
---
+++ scribblings/fractal-clock.scrbl
@@ -0,0 +1,12 @@
+#lang scribble/manual
+@require[@for-label[fractal-clock
+                    racket/base
+                    racket/date
+                    racket/gui]]
+
+@title{fractal-clock}
+@author{xiaoyu2006}
+
+@defmodule[fractal-clock]
+
+A simple fractal clock.
